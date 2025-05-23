5ba0771896aa264b08c13df5b84a0e58c2a0d882
☠︎༆SIGMALORD༆☠︎👿👿 <sigmalordnicks@gmail.com>
2025-05-24 00:36:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Merlin-MD~8ql2FJrD#_tnWllNlHP4a-9oUOYAhJ5z8Uvuzgn7mX_yuMmaBR34",
+SESSION_ID: process.env.SESSION_ID || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
