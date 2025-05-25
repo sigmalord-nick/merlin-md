c9e4a4d401503dfddde48d6e0024e4bf3a758ab5
☠︎༆SIGMALORD༆☠︎👿👿 <sigmalordnicks@gmail.com>
2025-05-25 15:02:11 +0300

    Create talkdrove.js

---
+++ talkdrove.js
@@ -0,0 +1,50 @@
+{
+  "bot-name": "merlin-XD",
+  "description": "ᴊᴀᴠᴀsᴄʀɪᴘᴛ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍerlin",
+  "logo": "https://files.catbox.moe/juxfce.jpg",
+  "keywords": ["bot"],
+  "documentation-link": "https://new-session-m3jx.onrender.com/",
+    "owner-verification":"sigmalordnicks@gmail.com",
+  
+  "env": {
+    "SESSION_ID": {
+      "description": "Must Start with 'merlin~'",
+      "required": true,
+      "value": ""
+    },
+      "BOT_NAME": {
+        "description": "write here your bot name.",
+        "required": true,
+        "value": "〠MERLIN〠"      
+    },  
+    "MODE": {
+      "description": "Set private/public/inbox/groups",
+      "required": false,
+      "value": "public"
+     },
+   
+    "PREFIX": {
+      "description": "Your Bot Prefix (default: .) You can put your own custom prefix",
+      "required": true,
+      "value": "."
+    },
+    
+    "OWNER_NAME": {
+      "description": "Put Owner Name Here",
+      "required": true,
+      "value": "ᴋɪɴɢ"
+    },
+    "OWNER_NUMBER": {
+      "description": "Put only one Owner Number Here",
+      "required": true,
+      "value": "254xxxxxx"
+    },
+    
+    "PRESENCE": {
+      "description": "Put online/typing/recording/unavailable",
+      "required": true,
+      "value": "online"
+
+    }
+  }
+}
