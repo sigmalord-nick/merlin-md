ea6c9344052d58c5feec4d5106cfe9e9afd6da94
☠︎༆SIGMALORD༆☠︎👿👿 <sigmalordnicks@gmail.com>
2025-05-25 15:32:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Merlin-MD~0r9imJJT#EIhBz8PWcmirDBMNgTZkb85GoU9le-71hdEd0pMoa5Q",
+SESSION_ID: process.env.SESSION_ID || "Merlin-MD~EuMQhDbY#U4JSJ8T8Imj7H5XlTEbnr6ruI_Hfm9yf-U0WN_KcAl8",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
